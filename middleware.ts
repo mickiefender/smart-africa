ae1c3d0db1470fdc5effbf249c55f9d130d5780e
Michael Coleman <107563286+mickiefender@users.noreply.github.com>
2025-09-19 10:25:03 +0000

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,29 @@
-import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
+import { createServerClient } from "@supabase/ssr"
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
 
-  // This automatically refreshes the session if needed
+  // Create a Supabase client scoped to middleware
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return req.cookies.getAll()
+        },
+        setAll(cookies) {
+          cookies.forEach(({ name, value, options }) =>
+            res.cookies.set(name, value, options)
+          )
+        },
+      },
+    }
+  )
+
+  // Refresh session if needed
   await supabase.auth.getSession()
 
   return res
