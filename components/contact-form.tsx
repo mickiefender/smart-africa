66ea341128cc74790a98bfa2985cf9de9d355a53
Mickie Fender <mickiefender@Mickies-MacBook-Air.local>
2026-03-19 23:25:45 +0000

    Number removed

--- components/contact-form.tsx
+++ components/contact-form.tsx
@@ -180,7 +180,7 @@ export default function ContactForm() {
                     </div>
                     <div>
                       <p className="font-medium text-foreground text-sm sm:text-base">Phone</p>
-                      <p className="text-muted-foreground text-sm sm:text-base"><a href="tel:+233208517482">+233 208 517 482</a><br /><a href="tel:+233206243442">+233 206 243 442</a><br /><a href="tel:+233200609004">+233 200 609 004</a></p>
+                      <p className="text-muted-foreground text-sm sm:text-base"><a href="tel:+233208517482">+233 208 517 482</a><br /><a href="tel:+233206243442">+233 206 243 442</a><br /></p>
                     </div>
                   </div>
                   <div className="flex items-center space-x-3">
